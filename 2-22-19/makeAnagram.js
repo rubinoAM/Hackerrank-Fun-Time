f104e8881804333a252588272399aa51188cd9b9
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-22 10:49:50 -0500

    Parsing thru string A

--- 2-22-19/makeAnagram.js
+++ 2-22-19/makeAnagram.js
@@ -1,13 +1,31 @@
 //Must return an integer representing the minimum total characters that must be deleted to make the strings anagrams
 
 function makeAnagram(a,b){
+    let mainCount = 0;
+    let charCount = 0;
+    let reversedB = b.split("").reverse().join("");
+
     for(let i=0;i<a.length;i++){
-        for(let j=0;j<b.length;j++){
-            if(a.charAt(i) == b.charAt(j)){
-                
+        for(let j=0;j<reversedB.length;j++){
+
+            if(a.charAt(i) != reversedB.substr(j,1)){
+                charCount++;
+            }
+
+            if(charCount == a.length){
+                a.charAt(i).replace(a.charAt(i),"");
+                charCount = 0;
+                mainCount++;
+            } else if (j == reversedB.length) {
+                charCount = 0;
             }
         }
     }
+
+    //console.log(a,reversedB);
+    console.log(mainCount);
 }
 
-makeAnagram('cde','dcf');
+makeAnagram('cde','dcf'); //Should return 4
+makeAnagram('fcrxzwscanmligyxyvym','jxwtrhvujlmrpdoqbisbwhmgpmeoke') //Should return 30
+makeAnagram('showman','woman'); //Should return 2
