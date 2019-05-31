007455e68fa6b69f310d327f9039ceddd060b65b
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-31 19:10:40 -0400

    Alternating Characters

---
+++ 5-31-19/alternatingChars.js
@@ -0,0 +1,8 @@
+function alternatingCharacters(s){
+    console.log(s);
+}
+
+const strings = ['AAAA','BBBBB','ABABABAB',"BABABA","AAABBB"];
+strings.map((str) => {
+    alternatingCharacters(str);
+})
