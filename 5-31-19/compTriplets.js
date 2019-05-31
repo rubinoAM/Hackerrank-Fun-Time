72451e9d743957308ee6fae10410ec7dc08bd7fb
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-31 18:46:54 -0400

    Comparing Triplets

--- 5-31-19/compTriplets.js
+++ 5-31-19/compTriplets.js
@@ -1,5 +1,18 @@
 function compareTriplets(a,b){
-    console.log(a,b);
+    let score = [];
+    let scoreA = 0;
+    let scoreB = 0;
+
+    for(let i=0;i<a.length;i++){
+        if(a[i] > b[i]){
+            scoreA += 1;
+        } else if (a[i] < b[i]){
+            scoreB += 1;
+        }
+    }
+
+    score.push(scoreA,scoreB);
+    console.log(score);
 }
 
 const a1 = [5,6,7];
