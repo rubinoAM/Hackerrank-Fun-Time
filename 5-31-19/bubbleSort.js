1565c567d77448e181daeb658db4e70ab960c96c
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-31 19:05:24 -0400

    Bubble Sort

--- 5-31-19/bubbleSort.js
+++ 5-31-19/bubbleSort.js
@@ -1,6 +1,17 @@
 function countSwaps(a){
-    console.log(a)
+    let count = 0;
+    for(let i=0;i<a.length;i++){
+
+        console.log(a);
+        for(let j=0;j<a.length-1;j++){
+            if(a[j] > a[j+1]){
+                [a[j], a[j+1]] = [a[j+1], a[j]];
+                count += 1;
+            }
+        }
+    }
+    console.log(count,a[0],a[a.length-1]);
 }
 
-const arr = [6,4,1];
+const arr = [3,1,0.5];
 countSwaps(arr);
