5ad199ed29c3d1783cf8e34f3210ac7f767d59e1
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-31 18:10:13 -0400

    Minimum Absolute Difference

--- 5-31-19/minAbDif.js
+++ 5-31-19/minAbDif.js
@@ -4,28 +4,32 @@ function minimumAbsoluteDifference(arr){
 
     for(let i=0; i<arr.length ; i++){
         for(let j=1; j<arr.length; j++){
-            if(Math.sign(arr[i]) === -1 | Math.sign(arr[i]) === -0){
+            if(Math.sign(arr[i]) === -1){
                 x = arr[i] * -1;
             } else {
                 x = arr[i];
             }
 
-            if(Math.sign(arr[j]) === -1 | Math.sign(arr[j]) === -0){
+            if(Math.sign(arr[j]) === -1){
                 y = arr[j] * -1;
             } else {
                 y = arr[j];
             }
-
-            z = x + y;
-            distances.push(z);
+            //console.log(x,y);
+            if(x !== y){
+                z = x + y;
+                distances.push(z);
+            }
         }
     }
     distances.sort((a,b) => { return a-b });
-    console.log(distances);
+    console.log(distances[0]);
 }
 
 const arr = [-59,-36,-13,1,-53,-92,-2,-96,-54,75];
-//minimumAbsoluteDifference(arr);
-
 const arrTwo = [3,-7,0];
-minimumAbsoluteDifference(arrTwo);
+const arrThree = [1,-3,71,68,17];
+
+//minimumAbsoluteDifference(arr);
+//minimumAbsoluteDifference(arrTwo);
+minimumAbsoluteDifference(arrThree);
